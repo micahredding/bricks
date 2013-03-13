6558185bcf7d3db4387ce81522207212c2f12aea
Micah Redding <micahtredding@gmail.com>
2013-03-13 17:36:07 -0500

    implement html as TPL instead of hard-coded module

---
+++ bricks_content.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/* An array called "content", consisting of field names and field content */
+?>
+<div class="block-content">
+  <?php foreach($content as $fieldname => $fieldcontent): ?>
+    <div class="block-<?php print $fieldname; ?>">
+      <?php print $fieldcontent; ?>
+    </div>
+  <?php endforeach; ?>
+</div>
